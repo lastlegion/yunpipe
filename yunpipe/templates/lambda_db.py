03ac64c43329f2c2a58c4adc59ccef61b8afc6ce
YX Wang <wangyx2005@gmail.com>
2016-10-15 18:29:46 -0400

    call lambda function to start ecs, need to deal with OUT seperately

--- yunpipe/templates/lambda_db.py
+++ yunpipe/templates/lambda_db.py
@@ -6,43 +6,45 @@ import json
 
 # needs to be generated
 required_steps = {}
+required_steps['sq'] = {'GLOBAL'}
 required_steps['sum'] = {'sq'}
 required_steps['count'] = {'sq'}
 required_steps['avg'] = {'sum', 'count'}
-required_steps['out'] = {'avg', 'sum', 'count'}
+required_steps['OUT'] = {'avg', 'sum', 'count'}
 
 # needs to be generated
 work_flow = {
     'inputs': {
-
+        'inp'
     },
     'outputs': {
-
+        'out'
     },
+    'intermediate_s3': '',
     'steps': {
         'sq': {
-            'inputs': {'': ''},
+            'inputs': {'inp': '#GLOBAL/inp'},
             'outputs': {'out'},
-            'sqs_arn': ''
+            'lambda_arn': ''
         },
         'sum': {
             'inputs': {'inp': '#sq/out'},
             'outputs': {'out'},
-            'sqs_arn': ''
+            'lambda_arn': ''
         },
         'count': {
             'inputs': {'inp': '#sq/out'},
             'outputs': {'out'},
-            'sqs_arn': ''
+            'lambda_arn': ''
         },
         'avg': {
             'inputs': {'sum': '#sum/out', 'count': '#count/out'},
             'outputs': {'out'},
-            'sqs_arn': ''
+            'lambda_arn': ''
         },
-        'out': {
-            'inputs': {},
-            'sqs_arn': ''
+        'OUT': {
+            'inputs': {'out': {'#GLOBAL/out'}},
+            'lambda_arn': ''
         }
     }
 }
@@ -90,9 +92,33 @@ def find_steps_to_start(newImage, newRecords):
 def start_docker_task(startList, newImage):
     for step in startList:
         info = {}
+        info['step'] = step
+        info['jobid'] = newImage['jobid']
+        if step == 'OUT':
+            # need to handle 'OUT' seperately
+            # info['copy']: list of files to be copyed
+            # info['delete']: list of files to be deleted
+
+            # TODO:
+            break
         for para in work_flow['steps'][step]['inputs']:
             tmp = work_flow['steps'][step]['inputs'][para].split('/')
             info[para] = newImage[tmp[0][1:]][tmp[1]]
-        # start ecs container
+
+        # call lambda function to start ecs
+        client = boto3.client('lambda')
+        response = client.invoke(FunctionName=work_flow[step]['lambda_arn'],
+                                 Payload=json.dumps(info).encode())
+        while(response['StatusCode'] != 200):
+            response = client.invoke(
+                FunctionName=work_flow[step]['lambda_arn'],
+                Payload=json.dumps(info).encode())
 
         # addapt from previous lambda_run_task_template.py
+
+
+old = {'jobid': '001'}
+new = {'jobid': '001', 'global': {'inp': {'global input'}}}
+
+new1 = {'jobid': '001', 'global': {'inp': {'global input'}},
+        'sq': {'out': {'sq output'}}}
