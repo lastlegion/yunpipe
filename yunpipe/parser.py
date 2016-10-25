10690023b1083002650a6dc6e178906c4a1dc1fa
YX Wang <wangyx2005@gmail.com>
2016-10-25 15:18:09 -0400

    add parse_workflow

--- yunpipe/parser.py
+++ yunpipe/parser.py
@@ -2,15 +2,13 @@ from yaml import safe_load
 from copy import deepcopy
 
 '''
-parse descriptions of single algorithmic step or workflow to a dictionary that
-is used internally
+parse descriptions of single algorithmic step or workflow to a dictionary that is used internally
 '''
 
 
-def cwl_parser(filepath):
+def parse_commandLineTool(filepath):
     '''
-    :para filepath: a cwl file to describe either single algorithmic step or a
-    workflow return a dictionary that used internally
+    :para filepath: a cwl file to describe single algorithmic step return a dictionary that used internally
     :type: file
 
     :rtype: dict
@@ -25,9 +23,49 @@ def cwl_parser(filepath):
             for entry in raw[key]:
                 info[key][entry['id']] = \
                     {i: entry[i] for i in entry if i != 'id'}
+        elif key == 'baseCommand':
+            info[key] = ' '.join(raw[key])
         elif key == 'hints':
             for i in raw[key]:
                 info[i] = deepcopy(raw[key][i])
         else:
             info[key] = deepcopy(raw[key])
     return info
+
+
+def parse_workflow(filepath):
+    '''
+    :para filepath: a cwl file to describe a workflow return a dictionary that used internally
+    :type: file
+
+    :rtype: dict
+    '''
+    with open('filepath') as f:
+        raw = safe_load(f)
+
+    info = {}
+    for key in raw:
+        if key == 'inputs' or key == 'outputs':
+            info[key] = {}
+            for entry in raw[key]:
+                info[key][entry['id']] = \
+                    {i: entry[i] for i in entry if i != 'id'}
+        elif key == 'steps':
+            for entry in raw[key]:
+                info[key][entry['id']] = {}
+                for k in entry:
+                    if k == 'inputs':
+                        info[key][entry['id']][k] = {}
+                        for e in entry[k]:
+                            if '/' in e['source']:
+                                info[key][entry['id']][k][e['id']] = \
+                                    e['source']
+                            else:
+                                info[key][entry['id']][k][e['id']] = \
+                                    '#GLOBAL/' + e['source'][1:]
+                    elif k == 'outputs':
+                        info[key][entry['id']][k] = set()
+                        for e in entry[k]:
+                            info[key][entry['id']][k].add(e['id'])
+                    elif k == 'run':
+                        info[key][entry['id']][k] = entry['run']
