17e0e6ef5924cc665c1cdd258b0ab7036d8d65c4
Yuxing Wang <wangyx2005@gmail.com>
2016-12-16 16:31:19 -0500

    add yunpipe_workflow_folder

--- yunpipe/__init__.py
+++ yunpipe/__init__.py
@@ -4,8 +4,10 @@ from .utils import get_full_path
 from .utils import create_folder
 
 CLOUD_PIPE_TMP_FOLDER = get_full_path('~/.cloud_pipe/tmp')
-CLOUD_PIPE_ALGORITHM_FOLDER = get_full_path('~/.cloud_pipe/algorithms')
+CLOUD_PIPE_ALGORITHM_FOLDER = get_full_path('~/.cloud_pipe/clTools')
+YUNPIPE_WORKFLOW_FOLDER = get_full_path('~/.cloud_pipe/workflow')
 CLOUD_PIPE_TEMPLATES_FOLDER = os.path.join(os.path.dirname(__file__), 'templates')
 
 create_folder(CLOUD_PIPE_TMP_FOLDER)
 create_folder(CLOUD_PIPE_ALGORITHM_FOLDER)
+create_folder(YUNPIPE_WORKFLOW_FOLDER)
