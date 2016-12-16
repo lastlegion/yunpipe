5370a2270e6e019a0d1449e276881e31616bf406
Yuxing Wang <wangyx2005@gmail.com>
2016-12-16 17:13:21 -0500

    add submit.py to submit job

--- yunpipe/pipeline/submit.py
+++ yunpipe/pipeline/submit.py
@@ -1,1 +1,34 @@
-from ..cwl_parser import 
+import sys
+from random import random
+
+from . import session
+from .set_pipe_with_db import gettime
+from ..cwl_parser import parse_job
+from ..utils import get_workflow_info
+
+
+db = session.resource('dynamodb')
+
+
+def main():
+    wf_name = sys.argv[1]
+    jobpath = sys.argv[2]
+
+    # TODO: based on wf_name find work_flow
+    wf = get_workflow_info(wf_name)
+    job = parse_job(jobpath)
+    jobid = gettime() + str(random())
+
+    table = db.Table(wf['db_table'])
+
+    table.put_item(Item={
+        'jobid': jobid,
+        'intermediate_s3': wf['intermediate_s3'],
+        'db_table': wf['db_table']
+    })
+
+    table.update_item(
+        Key={'jobid': jobid},
+        UpdateExpression='SET inputs = :val',
+        ExpressionAttributeValues={':val': job}
+    )
